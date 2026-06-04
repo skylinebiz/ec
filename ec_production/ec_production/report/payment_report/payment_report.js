33415086bb1a87241c32815613e4168fb703942f
Harshit Jain <jnharshit1998@gmail.com>
2026-06-04 07:01:28 +0000

    default months

--- ec_production/ec_production/report/payment_report/payment_report.js
+++ ec_production/ec_production/report/payment_report/payment_report.js
@@ -7,13 +7,15 @@ frappe.query_reports["Payment Report"] = {
             fieldname: "from_date",
             label: __("From"),
             fieldtype: "Date",
-            reqd: 1
+            reqd: 1,
+            default: frappe.datetime.month_start()
         },
         {
             fieldname: "to_date",
             label: __("To"),
             fieldtype: "Date",
-            reqd: 1
+            reqd: 1,
+            default: frappe.datetime.month_end()
         }
     ],
 
