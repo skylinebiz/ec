a412a7c99a07e3e439082b92b4b65e1c42210ce0
Harshit Jain <jnharshit1998@gmail.com>
2026-06-05 12:26:47 +0000

    block submitted process lo

--- ec_production/ec_production/doctype/ec_lot/ec_lot.py
+++ ec_production/ec_production/doctype/ec_lot/ec_lot.py
@@ -4,16 +4,49 @@
 import frappe
 from frappe.model.document import Document
 from frappe.utils import flt
+from frappe import _
 
 
 class ECLot(Document):
 
 	def validate(self):
 
-		self.total_qty = sum(
-			flt(row.qty)
-			for row in self.ec_lot_item
-		)
+		if self.is_new():
+			return
+
+		old_doc = self.get_doc_before_save()
+		if not old_doc:
+			return
+
+		if old_doc.ec_lot_item != self.ec_lot_item:
+			submitted_lots = frappe.get_all(
+				"EC Process Lot",
+				filters={"docstatus": 1},
+				pluck="name"
+			)
+
+			if submitted_lots:
+				process_lot = frappe.db.get_value(
+					"EC Process Lot Item",
+					{
+						"ec_lot": self.name,
+						"parent": ["in", submitted_lots]
+					},
+					"parent"
+				)
+
+				if process_lot:
+					frappe.throw(
+						_(
+							"Submitted Process Lot <a href='/app/ec-process-lot/{0}'>{0}</a> "
+							"exists against this EC Lot. Cancel the Process Lot before modifying this Lot."
+						).format(process_lot)
+					)
+				
+			self.total_qty = sum(
+				flt(row.qty)
+				for row in self.ec_lot_item
+			)
 
 		for row in self.ec_lot_item:
 
@@ -40,3 +73,14 @@ def get_rate(item, operation, date):
     )
 
     return flt(rate)
+
+
+def get_dashboard_data():
+    return {
+        "transactions": [
+            {
+                "label": "References",
+                "items": ["EC Process Lot"]
+            }
+        ]
+    }
